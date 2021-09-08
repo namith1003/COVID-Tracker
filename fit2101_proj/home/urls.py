1bfbf0b5042b4bd2835eea35dc766ca181fafb3d
Jie Yi Yew <jyew0001@student.monash.edu>
2021-09-08 19:44:36 +0800

    UI for signup,login and forgot password

--- fit2101_proj/home/urls.py
+++ fit2101_proj/home/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'), 
     path('logout', views.logout, name='logout'), 
     path('post/<str:pk>', views.post, name='post'), 
-    path('signup', views.signup, name="signup")
+    path('signup', views.signup, name="signup"),
+    path('forgotpassword', views.forgotpassword, name="forgotpassword"),
+
     ]
