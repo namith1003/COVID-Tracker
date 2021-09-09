e7841e172f44f0d9f467720067ffe8672f97ae68
Jonathan Yip <jyip0005@student.monash.edu>
2021-09-09 15:19:58 +0800

    s

--- fit2101_proj/home/urls.py
+++ fit2101_proj/home/urls.py
@@ -2,16 +2,18 @@ from django.urls import path
 from home import views
 app_name = 'home'
 urlpatterns = [
-    path('', views.index, name='index'), #when it is empty means it is root url
-    path('counter', views.counter, name='counter'), #when it is empty means it is root url
-    path('register', views.register, name='register'), 
-    path('login', views.login, name='login'), 
-    path('logout', views.logout, name='logout'), 
-    path('post/<str:pk>', views.post, name='post'), 
+    # when it is empty means it is root url
+    path('', views.index, name='index'),
+    # when it is empty means it is root url
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('post/<str:pk>', views.post, name='post'),
     path('signup', views.signup, name="signup"),
     path('forgotpassword/', views.forgotpassword, name="forgotpassword"),
 
-    ]
+]
 
 # urlpatterns = [
 #     # when it is empty means it is root url
