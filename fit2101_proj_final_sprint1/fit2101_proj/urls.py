f9233ae338fae6e0c9e0a6ab683f604c4968be19
Jonathan Yip <jyip0005@student.monash.edu>
2021-09-10 18:45:31 +0800

    merging_branch

---
+++ fit2101_proj_final_sprint1/fit2101_proj/urls.py
@@ -0,0 +1,31 @@
+"""fit2101_proj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+#template_name="registration/reset_password.html
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),
+         name='password_reset_complete'),
+]
---
+++ fit2101_proj_final_sprint1/home/views.py
@@ -0,0 +1,89 @@
+from django.http import request
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
+from home import urls
+from django.urls import reverse
+from django.core.mail import send_mail
+
+#from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        tel = request.POST['tel']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already registered')
+                return redirect('/register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already used')
+                return redirect('/register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.profile.tel = tel
+                user.save()
+                return redirect('/login')
+        else:
+            messages.info(request, 'Password does not match')
+            return redirect('/register')
+
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            print('hi')
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials are invalid')
+            print('hello')
+            return redirect('/login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    print(amount_of_words)
+    return render(request, 'counter.html', {'amount': amount_of_words})
+
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
+
+
+def add_widget(request):
+    pass
+
+
+def forget_password(request):
+        return render(request, 'forgotpassword.html')
+
+# def password_reset_request(request):
+#     return render(request, 'registrations/reset_password.html', context={})
