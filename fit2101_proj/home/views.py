8c54c9f78ef6548b76e0c34b6fd91cca89b217c3
Jonathan Yip <jyip0005@student.monash.edu>
2021-09-10 16:41:17 +0800

    send help

--- fit2101_proj/home/views.py
+++ fit2101_proj/home/views.py
@@ -1,11 +1,14 @@
-# from django.http import request
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User, auth
-# from django.contrib import messages
-# from .models import Profile
-from home import  urls
-from django.urls import reverse
-from django.http import HttpResponse
+from django.http import request
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
+
+
+
+# from home import  urls
+# from django.urls import reverse
+# from django.http import HttpResponse, render
 
 
 # Create your views here.
