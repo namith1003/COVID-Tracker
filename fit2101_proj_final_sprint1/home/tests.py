e81faa200105e859d75fd6f04b73da20bef727d7
namith <anim0003@student.monash.edu>
2021-09-13 00:09:48 +0530

    Made Testcases for signing up a user using the signup page and checking if the user exists and the page for signing up exists

--- fit2101_proj_final_sprint1/home/tests.py
+++ fit2101_proj_final_sprint1/home/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.test import TestCase
+from django.urls import reverse
+
 from .models import Profile
 
 
@@ -10,6 +12,10 @@ class ProfileTestCase(TestCase):
         self.user = User.objects.create_user('Namith', 'namithspider@gmail.com', 'mypass')
         self.user.save()
         self.profile = Profile(self.user, "0123456789")
+        self.username = 'testuser'
+        self.email = 'testuser@email.com'
+        self.telephone = '0123456789'
+        self.password = 'passwordForTesting'
 
     # tests if a newly created profile is of rank admin
     def test_profile_is_admin(self):
@@ -24,3 +30,26 @@ class ProfileTestCase(TestCase):
         user.set_password('new mypass')
         user.save()
         self.assertEqual(user.check_password("new mypass"), True)
+
+    def test_signup_page_url(self):
+        response = self.client.get("/register/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='register.html')
+
+    def test_signup_page_view_name(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='register.html')
+
+    def test_signup_form(self):
+        response = self.client.post('/register/', data={
+            'username': self.username,
+            'email': self.email,
+            'tel': self.telephone,
+            'password': self.password,
+            'password2': self.password
+        })
+        self.assertEqual(response.status_code, 302)
+
+        users = get_user_model().objects.all()
+        self.assertEqual(users.count(), 2)
